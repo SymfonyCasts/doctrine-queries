a3a0539e923a5a6a4a908d46c6f55d43dd6edacc
bocharsky-bw <bocharsky.bw@gmail.com>
2023-02-22 16:40:08 +0100

    Remove sensio/framework-extra-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
